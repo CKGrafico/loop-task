6c268262ca4cac6c400cd8518d0df70854ce41fe
Quique Fdez Guerra <quique@ckgrafico.com>
2026-07-09 17:21:42 +0200

    fx docs menu

--- docs/.source/server.ts
+++ docs/.source/server.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import * as __fd_glob_11 from "../content/docs/troubleshooting.mdx?collection=docs"
-import * as __fd_glob_10 from "../content/docs/task-chaining.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/troubleshooting.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/task-chaining.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/mcp-server.mdx?collection=docs"
 import * as __fd_glob_9 from "../content/docs/index.mdx?collection=docs"
 import * as __fd_glob_8 from "../content/docs/http-api.mdx?collection=docs"
 import * as __fd_glob_7 from "../content/docs/getting-started.mdx?collection=docs"
@@ -16,4 +17,4 @@ import type * as Config from '../source.config';
 
 const create = fromConfig<typeof Config>();
 
-export const docs = await create.docs("docs", "content/docs", {"meta.yaml": __fd_glob_0, }, {"agent-workflows.mdx": __fd_glob_1, "architecture.mdx": __fd_glob_2, "cli-reference.mdx": __fd_glob_3, "configuration.mdx": __fd_glob_4, "docker.mdx": __fd_glob_5, "examples.mdx": __fd_glob_6, "getting-started.mdx": __fd_glob_7, "http-api.mdx": __fd_glob_8, "index.mdx": __fd_glob_9, "task-chaining.mdx": __fd_glob_10, "troubleshooting.mdx": __fd_glob_11, });
+export const docs = await create.docs("docs", "content/docs", {"meta.yaml": __fd_glob_0, }, {"agent-workflows.mdx": __fd_glob_1, "architecture.mdx": __fd_glob_2, "cli-reference.mdx": __fd_glob_3, "configuration.mdx": __fd_glob_4, "docker.mdx": __fd_glob_5, "examples.mdx": __fd_glob_6, "getting-started.mdx": __fd_glob_7, "http-api.mdx": __fd_glob_8, "index.mdx": __fd_glob_9, "mcp-server.mdx": __fd_glob_10, "task-chaining.mdx": __fd_glob_11, "troubleshooting.mdx": __fd_glob_12, });
